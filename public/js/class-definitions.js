d312ab4985bd1a8ac6c3f1f8d933e3e69950dae1
Andrea <takamiya.andrea@gmail.com>
2016-12-04 14:00:10 -1000

    Rainbows tests passed

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -171,6 +171,18 @@ let domains = {
  *  isDouble  => true
  *
  */
+let rainbow = {
+  colors : {
+    red: "#F0280A",
+    orange: "#FF8800",
+    yellow: "#FFDD00",
+    green: "#51AB0C",
+    blue: "#1593ED",
+    indigo: "#5215ED",
+    violet: "#A915ED",
+  },
+  isDouble: true,
+};
 
 
 /* Step 14
