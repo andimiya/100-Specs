079b70df048890ab23200efc31004e217c7c6284
Andrea <takamiya.andrea@gmail.com>
2016-12-07 00:26:16 -1000

    Step 59 tests passed

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1169,6 +1169,11 @@ class Cookie {
  * @param {Array} foods All the foods in the meal
  */
 
+class Meal {
+  constructor(foods){
+    this.foods = foods;
+  }
+}
 
 /**
  * Create a new instance of the Classes you defined above, below!
